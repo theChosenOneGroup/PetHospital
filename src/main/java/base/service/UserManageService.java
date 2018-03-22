b382038186d92791bd05ec253e717d2b2b4d479f
罗新羽 <1349694179@qq.com>
2018-03-22 15:28:37 +0800

    bug fix

--- src/main/java/base/service/UserManageService.java
+++ src/main/java/base/service/UserManageService.java
@@ -4,11 +4,8 @@ import base.model.UserInfo;
 
 public interface UserManageService {
 
-<<<<<<< HEAD
-  public UserInfo registerUser(UserInfo userInfo);
+
   public UserInfo fixpassword(UserInfo userInfo);
-=======
   public int registerUser(UserInfo userInfo);
->>>>>>> temp1
 
 }
